7d89281490ba45a797db0bc05c842cd2344d9b1d
tuguzT <56771526+tuguzT@users.noreply.github.com>
2020-11-17 17:12:08 +0300

    Addition of Messenger fragment with top app bar

--- app/src/main/java/tntgroup/inlife/MainActivity.java
+++ app/src/main/java/tntgroup/inlife/MainActivity.java
@@ -2,13 +2,42 @@ package tntgroup.inlife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+/**
+ * Main activity class of application (subclass of {@link AppCompatActivity})
+ * with bottom navigation bar {@link BottomNavigationView}
+ */
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instance of bottom navigation view
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        // Set listener for select action
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                // "Love" was selected
+                case R.id.nav_love:
+                    break;
+                // "Search" was selected
+                case R.id.nav_search:
+                    break;
+                // "Messenger" was selected
+                case R.id.nav_messenger:
+                    break;
+                // "Settings" was selected
+                case R.id.nav_settings:
+                    break;
+            }
+            return true;
+        });
     }
 }
---
+++ app/src/main/java/tntgroup/inlife/MessengerFragment.java
@@ -0,0 +1,48 @@
+package tntgroup.inlife;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A subclass of {@link Fragment}
+ * "Messenger" window of application
+ */
+public class MessengerFragment extends Fragment {
+
+    /* Define parameters and their keys here */
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters
+     *
+     * @return A new instance of {@link MessengerFragment}
+     */
+    public static MessengerFragment newInstance(/* Parameters are defined here */) {
+        MessengerFragment fragment = new MessengerFragment();
+        Bundle args = new Bundle();
+        /* Put parameters into args Bundle object here */
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            /* Save data from bundle into instance of the class here */
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messenger, container, false);
+    }
+}
